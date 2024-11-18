afee623199c84a88b15e45891c3df132a374725d
alamhubb <alamhubb@163.com>
2024-11-18 09:13:42 +0800

    支持自动提示

--- src/ovs/factory/OvsAstToEstreeAstUtil.ts
+++ src/ovs/factory/OvsAstToEstreeAstUtil.ts
@@ -1,3 +1,25 @@
-export default class OvsAstToEstreeAstUtil {
+import {OvsAstProgram} from "../interface/OvsInterface";
+import {BaseNode} from "estree";
+import OvsParser from "../parser/OvsParser.ts";
 
+export default class OvsAstToEstreeAstUtil {
+    static toEstreeAst(program: OvsAstProgram) {
+
+    }
+
+    static createNodeAst(node: BaseNode) {
+        if (node.type === OvsParser.prototype.OvsRenderDomViewDeclaration.name) {
+
+        } else {
+
+        }
+    }
+
+    static ovsRenderDomViewDeclarationToEstree() {
+
+    }
+
+    static OvsRenderDomViewDeclaratorToEstree() {
+
+    }
 }
